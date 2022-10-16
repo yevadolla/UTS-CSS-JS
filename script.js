f7efd69f49db6b981030632cbd5c7325b11d9b0b
yevadolla <yevadolla@gmail.com>
2022-10-16 10:20:32 +0700

    js 1

---
+++ script.js
@@ -0,0 +1,20 @@
+var nama = prompt('Masukkan Nama Anda :');
+alert('Selamat Datang ' + nama + '^.^');
+
+// const judul = document.querySelector('navbar-brand');
+// judul.style.color = 'blue';
+
+// const judul = document.getElementsByName('a');
+
+//const mn = document.querySelector('#navbarNav');
+//const mnS = document.querySelector('.navbar-nav');
+
+function ubahWarna() {
+  document.querySelector('#nav-link').style.color = 'red';
+}
+// const ubahWarna = () => {
+//   mn.classList.toggle('is-active');
+//   mnS.classList.toggle('active');
+// };
+
+// mn.addEventListener('click, ubahWarna');
